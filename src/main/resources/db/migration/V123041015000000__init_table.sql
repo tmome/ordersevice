e2160960b650009ec47be7f8d351cce577e44af5
천현승 <woqkfehlfk@naver.com>
2023-04-10 15:47:49 +0900

    In memory 구성을 위한 init_table 추가

---
+++ src/main/resources/db/migration/V123041015000000__init_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS users (
+    id BIGINT(5) AUTO_INCREMENT PRIMARY KEY COMMENT '회원 식별자',
+    user_id VARCHAR(15) NOT NULL COMMENT '회원 아이디',
+    user_password VARCHAR(100) NOT NULL COMMENT '회원 비밀번호',
+    user_name VARCHAR(15) NOT NULL COMMENT '회원 아이디',
+    create_date DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP() COMMENT '생성일',
+    update_date DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP() COMMENT '수정일'
+);
+COMMENT ON TABLE users is '회원 테이블';
+
